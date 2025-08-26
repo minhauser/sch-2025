d90d9c26b2cb27a6ff6f74229084554f9181d66a
minhauser <7minhauser@gmail.com>
2025-08-26 11:48:25 +0900

    2025-08-26 commit~

--- springboot/src/main/java/com/sch/springboot/controller/SpringController.java
+++ springboot/src/main/java/com/sch/springboot/controller/SpringController.java
@@ -1,4 +1,22 @@
 package com.sch.springboot.controller;
 
-public class SpringController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+    public class SpringController {
+    @GetMapping("/spring")
+    public String spring(@RequestParam("name")  String name,
+                         @RequestParam("age") int age,
+                         Model model) {
+        System.out.print(name);
+        System.out.println(age);//서버 콘솔에 출력
+
+        //model 객체 추가 --> view 전달
+        model.addAttribute("name",name);
+        model.addAttribute("age",age);
+        return "spring"; //view name
+    }
 }
--- springboot/src/main/java/com/sch/springboot/controller/SpringbootApplication.java
+++ springboot/src/main/java/com/sch/springboot/controller/SpringbootApplication.java
@@ -1,4 +1,4 @@
-package domain;
+package com.sch.springboot.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
